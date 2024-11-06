642b5b603356d1ed1b28fe5cca98fcfa4e9ccd23
IldizovNodirbek <158564155+IldizovNodirbek@users.noreply.github.com>
2024-11-06 09:32:59 +0500

    Update SignIn.jsx

--- src/Registration/SignIn.jsx
+++ src/Registration/SignIn.jsx
@@ -24,30 +24,32 @@ function SignIn() {
   };
 
   return (
-    <div className=" border-gray-700 p-4">
-      <div className="flex">
-        <div className="flex items-center">
+    <div className="border-gray-700 p-4">
+      <div className="flex flex-col sm:flex-row items-center sm:items-start">
+        <div className="flex items-center mb-6 sm:mb-0">
           <img
             src={target}
             alt="arrow-svg-photo"
             className="w-[40px] h-[40px]"
           />
-          <h1 className="text-3xl ml-4 font-bold">Daily Tasks</h1>
+          <h1 className="text-2xl sm:text-3xl ml-4 font-bold">Daily Tasks</h1>
         </div>
         <div className="flex ml-auto">
           <Link
             to="/signup"
-            className="bg-black text-white px-8 py-2 rounded hover:bg-gray-700"
+            className="bg-black text-white px-6 py-2 rounded hover:bg-gray-700"
           >
             Sign Up
           </Link>
         </div>
       </div>
-      <div className="mt-28 border border-gray-300 p-8 max-w-2xl mx-auto">
-        <h2 className="text-2xl font-semibold">Sign In</h2>
-        <p className="text-sm mb-6 mt-2">
+
+      <div className="mt-16 sm:mt-28 border border-gray-300 p-6 sm:p-8 max-w-lg mx-auto">
+        <h2 className="text-xl sm:text-2xl font-semibold">Sign In</h2>
+        <p className="text-sm mb-4 sm:mb-6 mt-2">
           Welcome back! Please enter your email to sign in.
         </p>
+
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
             <label className="block text-sm font-medium mb-2" htmlFor="email">
@@ -62,9 +64,10 @@ function SignIn() {
               className="w-full px-4 py-2 border border-gray-300 rounded outline-none focus:ring-2 focus:ring-blue-500"
             />
           </div>
+
           <button
             type="submit"
-            className="bg-black text-white px-4 py-2 rounded hover:bg-gray-700"
+            className="bg-black text-white w-full sm:w-auto px-8 py-2 rounded hover:bg-gray-700"
           >
             Sign In
           </button>
